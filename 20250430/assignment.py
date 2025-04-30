db1b2de32c886cb49cab3618a5d1f37b29fc7432
hyoeun.jeong <jhe0316@naver.com>
2025-04-30 18:45:00 +0900

    자습습

---
+++ 20250430/assignment.py
@@ -0,0 +1,35 @@
+#6053
+#a=input()
+#a=bool(int(a))
+#print(not a)
+#참 거짓 바꾸기
+
+#6054
+#a,b=input().split()
+#a,b =int(a),int(b)
+#print(bool(int(a)) and bool(int(b)))
+
+#6055
+#a,b =input().split()
+#a,b =int(a),int(b)
+#print(bool(int(a))or bool(int(b)))
+
+#6056
+
+#a,b =input().split()
+#a,b =bool(int(a)),bool(int(b) )
+
+#print((a and(not b)) or ((not a)and b))
+
+#6057
+
+#a,b =input().split()
+#a,b =bool(int(a)),bool(int(b) )
+#print((a and b)or ((not a)and (not b)))
+
+#6058
+#a,b =input().split()
+#a,b= bool(int(a)),bool(int(b))
+#print(not(a or b))
+
+#606
