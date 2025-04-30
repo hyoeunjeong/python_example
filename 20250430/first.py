eb047b68e9717ec02fd0a73005d29bdda251a40b
hyoeun.jeong <jhe0316@naver.com>
2025-04-30 12:49:40 +0900

    20250430 오전수업

--- 20250430/first.py
+++ 20250430/first.py
@@ -1,1 +1,118 @@
 # 20250430 오전강의
+
+#a =9
+#b =21
+
+#if a>10:
+ #   print("good")
+#elif b == 20: #if 첫 if 가 거짓이면 elif 로 들어간다.
+#    print("20입니다.")
+#elif a ==10:
+#    print("10입니다.")  #값이 20입니다 나옴
+#else: 
+  #print("이도저도 아니다")
+'''
+    20나오는 이유 코드는 위에서 아래 로 읽는다 즉 위에 실행되는 
+    if가 있다면 그것 이 실행된다.
+    
+ '''
+
+    #조건문에서 이도저도 아닌 상황은 --->  else 를 사용한다.   
+
+    
+#split() 활용
+#a,b 변수를 활용,
+#키,몸무게를 입력받는다.
+
+#키와 몸무게를 나눈 나머지를 출력한다.
+
+#조건문을 활용해서
+
+#키(a)가 130 이상이면 a, 150이상이면 b, 170 이상이면 c 180 이상면이면 d를 출력하세요요 
+
+
+#print("키 와 몸무게를 입력하시오")
+
+#answer=input().split(" ")
+#cm,kg =int(answer[0]),int(answer[1])
+
+#c=cm % kg
+
+#print(c)
+#if cm >= 130:
+#    print("a")
+#elif cm >= 150:
+#    print("b")
+#elif cm >= 170:
+#    print("c")
+#elif cm >= 180:
+#    print("d")
+
+
+#a= 185
+
+#f a>= 130:
+ #  print("130 이상입니다.")
+
+#if a>= 180:
+#    print("d")
+#elif a>=170:
+# print("c")
+#elif a>=150:
+# print("b")
+#else:
+#  print("a")
+
+
+#score=input("점수를 입력하세요.")
+#score =int(score)
+
+#score =int(input("점수를 입력해주세요"))
+#if 90<= score <=100 :
+#    print("A")
+#elif 80<= score <=89:
+#    print("B")
+#elif 70<= score <=79:
+#    print("C")
+#elif 60<= score <=69:
+#     print("D")
+#else:
+#    print("F")
+
+#들여쓰기 오류나는 이유
+#why??? 콜론 뒤(들여쓰기 블록 )
+#뒤에  주석처리를 한다면 말이 안된다.--> 즉 에러발생
+
+#and or 연산활용
+
+#a=10
+#b=20
+
+#if a ==10  and b ==20:
+ #print("good")
+
+#else:
+ #print("no")
+
+'''
+a,b,c 를 입력받는다.
+a가 100이고 b가 200이상이면 "a"를 출력
+b가 1이면 "b"를 출력
+이도저도 아니면 c를 출력
+'''
+print("숫자를 입력해주세요")
+a,b,c = input().split(" ")  
+#강사님예시 a,b,c =int(input()),int(input()),int(input())
+a,b,c=int(a),int(b),int(c)
+
+if a ==100 and b >=200:
+    print("a")
+
+elif b ==1 :
+    print("b")
+
+else :
+ print("c")
+
+ #elif로 마무리 해도 가능 하지만 만약 참이 아닌경우 거짓이 나왔을때 처리가 어려움
+ #가능은 하지만 (이도저도 아닐떄)힘듬---> 요구사항에 따라 달라지긴한다다
