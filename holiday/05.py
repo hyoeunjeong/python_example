c9e0d76acf28cb60b438c23c7b636b0b41286660
hyoeun.jeong <jhe0316@naver.com>
2025-05-04 14:19:39 +0900

    05

---
+++ holiday/05.py
@@ -0,0 +1,27 @@
+## 실용문제 (41-50)
+
+##41 : 비밀번호 강도 검사 
+#print("비밀번호를 입력하시오")
+#password =input()
+#len_ok =len(password) >= 8
+#number =any(str.isdigit() for str in password) ##숫자포함 여부부
+#word =any(str.isalpha() for str in password) ##문자포함여부부
+#if (len_ok) and (number) and (word ):
+ #   print("안전한 비밀 번호 입니다.")
+#else :
+ #   print("안전하지 않은은 비밀 번호 입니다.")
+
+
+
+##42:단어 뒤집기
+#print("문장을 입력하시오")
+#text =input().split()
+#str1 =text[0]
+#str2 =text[1]
+#str3 =text[2]
+
+#print(f"{str1[::-1]} {str2[::-1]} {str3[::-1]}")
+
+##43:문자 카운터
+
+
