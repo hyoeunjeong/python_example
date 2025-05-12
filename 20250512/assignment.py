1dc0822594d6a98d36f1116862884bae24dee41e
hyoeun.jeong <jhe0316@naver.com>
2025-05-12 17:27:59 +0900

    과제제

---
+++ 20250512/assignment.py
@@ -0,0 +1,69 @@
+##1.
+# 문제1: 두 수를 더하는 함수를 만드세요.
+def solution(a, b):
+ return  a + b 
+print(solution(5, 3))  # 결과: 8
+
+##2
+# 문제2: 두 수를 곱하는 함수를 만드세요.
+def solution(a, b):
+ return a*b
+print(solution(4, 7))  # 결과: 28
+
+
+##3
+# 문제3: 리스트의 모든 숫자를 더하는 함수를 만드세요.
+
+def solution(numbers):
+    return sum(numbers)
+
+print(solution([1, 2, 3, 4, 5]))  # 출력: 15
+
+
+##4
+
+# 문제4: 리스트의 평균값을 계산하는 함수를 만드세요.
+def solution(numbers):
+    return sum(numbers)/len(numbers)
+
+print(solution([10, 20, 30, 40]))  # 결과: 25.0
+
+
+##5
+
+# 문제5: 문자열의 길이를 반환하는 함수를 만드세요.
+def solution(text):
+    return len(text)
+
+print(solution("파이썬"))  # 결과: 3
+
+
+##6
+
+# 문제6: 리스트에서 가장 큰 수를 찾는 함수를 만드세요.
+def solution(numbers):
+    return max(numbers)
+
+print(solution([7, 12, 3, 8, 4]))  # 결과: 12
+
+##7
+# 문제7: 리스트에서 가장 작은 수를 찾는 함수를 만드세요.
+def solution(numbers):
+    return min(numbers)
+
+print(solution([7, 12, 3, 8, 4]))  # 결과: 3
+
+##8
+# 문제8: 문자열을 n번 반복하는 함수를 만드세요.
+def solution(text, n):
+    return text*n
+
+solution("안녕", 3)  # 결과: "안녕안녕안녕"
+
+##9 
+# 문제9: 주어진 숫자가 짝수인지 확인하는 함수를 만드세요. (짝수면 True, 홀수면 False 반환)
+def solution(number):
+    return number %2 ==0
+
+print(solution(6))  # 결과: True
+print(solution(9))  # 결과: False
